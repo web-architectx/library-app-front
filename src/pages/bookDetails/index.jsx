b45f8ea90a20adbfbdc1d7a8bff9e4686e8ece4d
Sunday-Binakin <sundaybinakin@gmail.com>
2024-10-09 02:00:20 +0000

    book details added

--- src/pages/bookDetails/index.jsx
+++ src/pages/bookDetails/index.jsx
@@ -1,24 +1,42 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+import Aragon from '../../assets/images/books/aragon.jpg'
 
 const BookDetails = () => {
   return (
     <div>
-        <Navbar/>
-        <Navbar />
-      <div className='flex flex-row bg-slate-900 justify-center'>
-        <div className='h-[100vh] w-[40vw] bg-yellow-400 mt-12 mb-24'>
+      <Navbar />
 
+      <div className='flex flex-row bg-[#626262] justify-center gap-6 '>
+      {/* <div className='flex flex-row  bg-red-400 justify-center'> */}
+        <div className='h-[75vh] w-[25%]  mt-32'>
+          <img src={Aragon} alt="img" className='w-full h-full object-cover' />
         </div>
-        <div className='h-[100vh] w-[40vw] bg-red-400  mt-12 mb-24'>
-
+        <div className='h-[100vh] w-[60%]   mt-32 mb-24'>
+          <h1 className='text-[40px] text-white'>Steve Jobs</h1>
+          <h4 className='text-[20px] text-white mt-6'>Year <span>:</span>2011</h4>
+          <h4 className='text-[20px] text-white mt-6'>Publisher <span>:</span>Hachette Digital</h4>
+          <h4 className='text-[20px] text-white mt-6'>Summary <span>:</span>Based on more than forty interviews with Steve Jobs conducted over two years - as well as interviews with more than a hundred family members, friends, adversaries, competitors, and colleagues - this is the acclaimed, internationally bestselling biography of the ultimate icon of inventiveness. Walter Isaacson tells the story of the rollercoaster life and searingly intense personality of creative entrepreneur whose passion for perfection and ferocious drive revolutionized six industries: personal computers, animated movies, music, phones, tablet computing, and digital publishing.</h4>
+          <div className='flex flex-row gap-6 mt-8'>
+            <h4 className='text-white text-[20px]'>Categories</h4>
+            <button className=" w-[7vw] px-4 py-2 text-white bg-[#E54224] bg-opacity-70 hover:bg-opacity-90 rounded-md border border-[#E54224] transition-colors duration-300">
+              <a href="#">Fiction</a>
+            </button>
+          </div>
+          <div className='mt-6 flex flex-row gap-4'>
+          <button className=" w-[7vw] px-4 py-2 text-white bg-[#E54224] bg-opacity-70 hover:bg-opacity-90 rounded-md border border-[#E54224] transition-colors duration-300">
+              <a href="#">Edit</a>
+            </button>
+            <button className=" w-[7vw] px-4 py-2 text-white bg-[#E54224] bg-opacity-70 hover:bg-opacity-90 rounded-md border border-[#E54224] transition-colors duration-300">
+              <a href="#">Delete</a>
+            </button>
+          </div>
         </div>
 
       </div>
-      {/* <p>book details</p> */}
+
       <Footer />
-        <Footer/>
     </div>
   )
 }
--- src/pages/bookList/index.jsx
+++ src/pages/bookList/index.jsx
@@ -1,27 +1,39 @@
 // import aragon from '../assets/images/books/aragon.jpg'
-// import K from '../constants/index'
+
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Navbar";
+
 import K from "../../constants";
+import Hero from "./components/Hero";
+<Navbar />
 
 const BookList = () => {
-  return (
-    <div className='grid grid-cols-4 w-2/5 mx-auto my-auto my-20 gap-12'>
-    {
-        K.OFFERS.map((offer, index) => {
-            console.log(`${index}: ${offer.author}`);
-            return(
-            <div key={index} className='flex flex-col gap-y-2 mt-8'>
-                <span className='p-2 text-2xl text-white rounded-[10px]'>
-                    <img src={offer.image} alt="img" className="w-[60vw] h-[20vh]" />
-                </span>
-                <p className="text-[#E54324]">{offer.title}</p>
-                <h3 className='text-[15px] font-semibold  '>{offer.author}</h3>
-               
-            </div>
-            )
-        })
-    }
-</div>
-  )
-}
+    return (
+        <>
+        <Navbar/>
+        <Hero/>
+        <div className="grid grid-cols-4 w-4/5 mx-auto my-auto my-20 gap-12 mt-8">
+            {
+                K.OFFERS.map((offer, index) => {
+                    console.log(`${index}: ${offer.author}`);
+                    return (
+                        <div key={index} className="flex flex-col items-center gap-y-4 p-4 bg-[#1E1E1E] rounded-lg shadow-lg">
+                            <span className="block rounded-lg overflow-hidden">
+                                <img src={offer.image} alt="img" className="w-[250px] h-[350px] object-cover transition-transform duration-300 hover:scale-105" />
+                            </span>
+                            <p className="text-lg font-semibold text-[#E54224]">{offer.title}</p>
+                            <h3 className="text-base text-white font-medium">{offer.author}</h3>
+                        </div>
+                    );
+                })
+            }
+        </div>
+        <Footer/>
+        </>
+    );
+};
+
+
+
 
 export default BookList
